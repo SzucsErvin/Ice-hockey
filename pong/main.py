a443617376bf795d7d90eb6009f5a5446dd6b13a
Ervin Szűcs <szucservin0@gmail.com>
2023-01-12 11:13:12 +0100

    collide fix, sound

--- pong/main.py
+++ pong/main.py
@@ -1,23 +1,46 @@
 import pygame, sys, random
 
 def ball_animation():
+
     global ball_speed_x, ball_speed_y, player_score, opponent_score, score_time
+   
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
+        pygame.mixer.Sound.play(hit_sound)
         ball_speed_y *= -1
     
+    #player score
     if ball.left <= 0:
+        pygame.mixer.Sound.play(score_sound)
         player_score += 1
         score_time = pygame.time.get_ticks()
-
+    
+    #opponent score
     if ball.right >= screen_width:
+        pygame.mixer.Sound.play(score_sound)
         opponent_score += 1
         score_time = pygame.time.get_ticks()
 
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        ball_speed_x *= -1
+    if ball.colliderect(player) and ball_speed_x > 0:
+        pygame.mixer.Sound.play(hit_sound)
+        if abs(ball.right - player.left) < 10:
+            ball_speed_x *= -1
+        elif abs(ball.bottom - player.top) < 10 and ball_speed_y > 0:
+            ball_speed_y *= -1
+        elif abs(ball.top - player.bottom) < 10 and ball_speed_y < 10:
+            ball_speed_y *= -1
+    
+    if ball.colliderect(opponent) and ball_speed_x < 0:
+        pygame.mixer.Sound.play(hit_sound)
+        if abs(ball.left - opponent.right) < 10:
+            ball_speed_x *= -1
+        elif abs(ball.bottom - opponent.top) < 10 and ball_speed_y > 0:
+            ball_speed_y *= -1
+        elif abs(ball.top - opponent.bottom) < 10 and ball_speed_y < 10: 
+            ball_speed_y *= -1
+        
 
 def player_animation():
     player.y += player_speed
@@ -91,10 +114,13 @@ opponent_speed = 7
 # text variables
 player_score = 0
 opponent_score = 0
-game_font = pygame.font.Font("freesansbold.ttf", 32)
+basic_font = pygame.font.Font("freesansbold.ttf", 32)
 
 # score timer
 score_time = True
+#sound
+hit_sound = pygame.mixer.Sound("pong.ogg")
+score_sound = pygame.mixer.Sound("score.ogg")
 
 while True:
     #handling input
